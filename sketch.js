2d97609c3282888b3af95546813a1c69f7a5d632
cheryl1908 <73298909+cheryl1908@users.noreply.github.com>
2021-02-06 17:57:07 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
 var dog,happyDog,foodS,foodStock,database;
-
 function preload()
 {
   dogImg=loadImage("images/dogImg.png");
@@ -16,21 +15,34 @@ function setup() {
   foodStock=database.ref("Food");
   foodStock.on("value",readStock);
 
-if(keyDown(UP_ARROW)){
-  writeStock(foodS);
-  dog.addImage(dogHap);
-}
 }
 function draw() {  
   background(46,139,87);
-  drawSprites();
 
+  if(keyWentDown(UP_ARROW)){
+    console.log("insideIf")
+    writeStock(foodS);
+    dog.addImage(dogHap);
+  }
+
+  drawSprites();
+  fill(255,255,254);
+   stroke("black"); 
+   text("Food remaining : "+foodS,170,200); 
+   textSize(13); 
+   text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
 }
-function readStock(){
-  foodS=database.val;
+function readStock(data){
+  foodS=data.val();
 }
-function writeStock(){
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  }
   database.ref('/').update({
     Food:x
+  
   })
 }
